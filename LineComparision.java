1173af22061900a0d43248c8ad431775ba17351c
Mani1358 <mani621998@gmail.com>
2023-01-24 13:00:53 +0530

    Added:UC-2

--- LineComparision.java
+++ LineComparision.java
@@ -14,7 +14,21 @@ public class LineComparision {
         int x2 = sc.nextInt();
         int y2 = sc.nextInt();
         l1 = (Double) Math.sqrt((x1 - x2) * (x1 - x2) + (y2 - y1) * (y2 - y1));
-        System.out.println("Length of len1: " + l1);
+        System.out.println("Length of len1: " + l1);System.out.println("Enter second line co-ordinates ,first points(x12,y12): ");
+        int x12 = sc.nextInt();
+        int y12 = sc.nextInt();
+        System.out.println("Enter second line co-ordinates ,first points(x22,y22): ");
+        int x22 = sc.nextInt();
+        int y22 = sc.nextInt();
+        l2 = (Double) Math.sqrt((x12 - x22) * (x12 - x22) + (y22 - y12) * (y22 - y12));
+        System.out.println("Length of len2: "+l2);
+
+        boolean out = (l1.equals(l2));
+        if(out==true){  // Using Equals method to check both lines are equal
+            System.out.println("Both lines are Equal");
+        } else {
+            System.out.println("Both lines are not Equal");
+        }
     }
 }
 
